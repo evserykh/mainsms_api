cdfc0f4050e88a90a747e64b8f9fb9d76e902cb2
Константин Иванов <kewogc@gmail.com>
2016-05-12 16:17:05 +0600

    fix sorting

--- lib/mainsms_api.rb
+++ lib/mainsms_api.rb
@@ -4,10 +4,11 @@ require 'active_support/core_ext/string'
 require 'hashie'
 require 'httparty'
 
+require 'mainsms_api/configuration'
+require 'mainsms_api/common'
+
 require 'mainsms_api/balance'
 require 'mainsms_api/cancel'
-require 'mainsms_api/common'
-require 'mainsms_api/configuration'
 require 'mainsms_api/info'
 require 'mainsms_api/message'
 require 'mainsms_api/price'
